d827b143bf22ba4864a14bf95f6d45b24bfea6eb
Mahdi <mahdi.ashuri14@gmail.com>
2024-11-13 13:04:51 +0100

    lister les eleve

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Instrument;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 
 class InstrumentController extends AbstractController
 {
@@ -14,5 +17,14 @@ class InstrumentController extends AbstractController
         return $this->render('instrument/index.html.twig', [
             'controller_name' => 'InstrumentController',
         ]);
+    }
+
+    public function lister(ManagerRegistry $doctrine)
+    {
+        $instrument = $doctrine->getRepository(Instrument::class)->findAll();
+
+        return $this->render('instrument/lister.html.twig', [
+            'pInstrument' => $instrument,
+        ]);	
     }
 }
