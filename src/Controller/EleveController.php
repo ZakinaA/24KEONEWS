dc2420069a0c5ee11d83d7285354a09b6da4f951
LeoMEURDRA <leo.meurdra23@gmail.com>
2024-11-13 12:37:19 +0100

    Ajout de la fonction lister au controlleur EleveController pour permettre d'afficher la liste des eleves sur une page

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -5,11 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Eleve;
 
 class EleveController extends AbstractController
 {
-    public function index(): Response
-    {
+    public function lister(ManagerRegistry $doctrine) {
+        $eleves = $doctrine->getRepository(Eleve::class)->findAll();
+
+        return $this->render('eleve/lister.html.twig', [
+            'pEleves' => $eleves,
+        ]);	
+    }
+
+    public function index(): Response {
         return $this->render('eleve/index.html.twig', [
             'controller_name' => 'EleveController',
         ]);
