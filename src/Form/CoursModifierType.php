b6a114e4098da8d6a286a6a541d69b70cbf1a72b
Anthony <anthony.negrier3@gmail.com>
2024-11-18 13:46:56 +0100

    Creation de CoursModifierType et de la page twig modifier

--- src/Form/CoursModifierType.php
+++ src/Form/CoursModifierType.php
@@ -5,14 +5,15 @@ namespace App\Form;
 use App\Entity\Cours;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
-class CoursType extends AbstractType
+
+class CoursModifierType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -21,9 +22,12 @@ class CoursType extends AbstractType
             ->add('ageMini')
             ->add('heureDebut')
             ->add('heureFin')
-            ->add('enregistrer', SubmitType::class, array('label' => 'Nouveau cours'))
-            
+            ->add('enregistrer', SubmitType::class, array('label' => 'Modifier cours'))
         ;
+    }
+
+    public function getParent(){
+        return CoursType::class;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -32,4 +36,5 @@ class CoursType extends AbstractType
             'data_class' => Cours::class,
         ]);
     }
+
 }
