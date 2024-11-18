b2f4a4dcdff27c9091981c3e15e717e54069effd
LeoMEURDRA <leo.meurdra23@gmail.com>
2024-11-18 10:08:08 +0100

    Ajout de la fonction consulter au controlleur EleveController pour permettre de consulter un eleve sur une page

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -7,9 +7,16 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Eleve;
+use Symfony\Component\HttpFoundation\Request;
 
 class EleveController extends AbstractController
 {
+    public function index(): Response {
+        return $this->render('eleve/index.html.twig', [
+            'controller_name' => 'EleveController',
+        ]);
+    }
+
     public function lister(ManagerRegistry $doctrine) {
         $eleves = $doctrine->getRepository(Eleve::class)->findAll();
 
@@ -18,9 +25,17 @@ class EleveController extends AbstractController
         ]);	
     }
 
-    public function index(): Response {
-        return $this->render('eleve/index.html.twig', [
-            'controller_name' => 'EleveController',
+    public function consulter(ManagerRegistry $doctrine, int $id){
+        $eleve= $doctrine->getRepository(Eleve::class)->find($id);
+
+        if (!$eleve) {
+            throw $this->createNotFoundException(
+            'Aucun eleve trouvé avec le numéro '.$id
+            );
+        }
+
+        return $this->render('eleve/consulter.html.twig', [
+            'eleve' => $eleve,
         ]);
     }
 }
