4de383b6e18788814426e7d7f0d156fbb75664f9
LeoMEURDRA <leo.meurdra23@gmail.com>
2024-11-27 08:32:43 +0100

    Modification du formulaire ResponsableModifierType pour le rendre indépendant de ResponsableType

--- src/Form/ResponsableModifierType.php
+++ src/Form/ResponsableModifierType.php
@@ -17,9 +17,19 @@ class ResponsableModifierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->remove('ajouter')
-            ->add('nom', TextType::class, array('label' => 'Nom Élève', 'disabled'=> true))
-            ->add('enregistrer', SubmitType::class, array('label' => 'Modifier Élève'));
+        ->remove('enregistrer')
+        ->add('nom', TextType::class, [
+            'label' => 'Nom Responsable', 
+            'disabled' => true,
+        ])
+        ->add('prenom', TextType::class, ['label' => 'Prénom Responsable'])
+        ->add('numRue', IntegerType::class, ['label' => 'Numéro de Rue'])
+        ->add('rue', TextType::class, ['label' => 'Rue'])
+        ->add('copos', IntegerType::class, ['label' => 'Code Postal'])
+        ->add('ville', TextType::class, ['label' => 'Ville'])
+        ->add('tel', IntegerType::class, ['label' => 'Téléphone'])
+        ->add('mail', TextType::class, ['label' => 'Email'])
+        ->add('enregistrer', SubmitType::class, ['label' => 'Modifier Responsable']);
     }
 
     public function getParent(){
