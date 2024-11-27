10f38a39f21a8739ce259075af78bc2999be18eb
BrianDUPONT <brian-dupont@hotmail.com>
2024-11-27 08:31:09 +0100

    Correction de fautes d'accords dans les exceptions

--- src/Controller/ProfessionnelController.php
+++ src/Controller/ProfessionnelController.php
@@ -27,84 +27,81 @@ class ProfessionnelController extends AbstractController
 
         return $this->render('professionnel/lister.html.twig', [
             'pProfessionnels' => $professionnels,
-        ]);	
+        ]);
     }
 
-        public function consulter(ManagerRegistry $doctrine, int $id)
+    public function consulter(ManagerRegistry $doctrine, int $id)
     {
         $professionnel = $doctrine->getRepository(Professionnel::class)->find($id);
 
         if (!$professionnel) {
             throw $this->createNotFoundException(
-                'Aucune professionnel trouvé avec le numéro '.$id
+                'Aucun professionnel trouvé avec le numéro ' . $id
             );
         }
-        
+
         return $this->render('professionnel/consulter.html.twig', [
             'professionnel' => $professionnel,
         ]);
     }
 
-    public function ajouter(ManagerRegistry $doctrine,Request $request){
+    public function ajouter(ManagerRegistry $doctrine, Request $request)
+    {
         $professionnel = new professionnel();
-	$form = $this->createForm(professionnelType::class, $professionnel);
-	$form->handleRequest($request);
- 
-	if ($form->isSubmitted() && $form->isValid()) {
- 
+        $form = $this->createForm(professionnelType::class, $professionnel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
             $professionnel = $form->getData();
- 
+
             $entityManager = $doctrine->getManager();
             $entityManager->persist($professionnel);
             $entityManager->flush();
- 
-	    return $this->render('professionnel/consulter.html.twig', ['professionnel' => $professionnel,]);
-	}
-	else
-        {
-            return $this->render('professionnel/ajouter.html.twig', array('form' => $form->createView(),));
-	}
+
+            return $this->render('professionnel/consulter.html.twig', ['professionnel' => $professionnel,]);
+        } else {
+            return $this->render('professionnel/ajouter.html.twig', array('form' => $form->createView(), ));
+        }
     }
 
-    public function modifier(ManagerRegistry $doctrine, $id, Request $request){
- 
+    public function modifier(ManagerRegistry $doctrine, $id, Request $request)
+    {
+
         //récupération de l'étudiant dont l'id est passé en paramètre
         $professionnel = $doctrine->getRepository(Professionnel::class)->find($id);
-     
-        if (!$professionnel) {
-            throw $this->createNotFoundException('Aucune professionnel trouvé avec le numéro '.$id);
-        }
-        else
-        {
-                $form = $this->createForm(ProfessionnelModifierType::class, $professionnel);
-                $form->handleRequest($request);
-     
-                if ($form->isSubmitted() && $form->isValid()) {
-     
-                     $professionnel = $form->getData();
-                     $entityManager = $doctrine->getManager();
-                     $entityManager->persist($professionnel);
-                     $entityManager->flush();
-                     return $this->render('professionnel/consulter.html.twig', ['professionnel' => $professionnel,]);
-               }
-               else{
-                    return $this->render('professionnel/modifier.html.twig', array('form' => $form->createView(),));
-               }
-            }
-     }
 
-     public function supprimer(ManagerRegistry $doctrine, int $id): Response
-     {
-         $professionnel = $doctrine->getRepository(Professionnel::class)->find($id);
- 
-         if (!$professionnel) {
-             throw $this->createNotFoundException('Aucune professionnel trouvé avec l\'ID '.$id);
-         }
- 
-         $entityManager = $doctrine->getManager();
-         $entityManager->remove($professionnel); 
-         $entityManager->flush();
- 
-         return $this->redirectToRoute('app_professionnel_lister');
-     }
+        if (!$professionnel) {
+            throw $this->createNotFoundException('Aucun professionnel trouvé avec le numéro ' . $id);
+        } else {
+            $form = $this->createForm(ProfessionnelModifierType::class, $professionnel);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $professionnel = $form->getData();
+                $entityManager = $doctrine->getManager();
+                $entityManager->persist($professionnel);
+                $entityManager->flush();
+                return $this->render('professionnel/consulter.html.twig', ['professionnel' => $professionnel,]);
+            } else {
+                return $this->render('professionnel/modifier.html.twig', array('form' => $form->createView(), ));
+            }
+        }
+    }
+
+    public function supprimer(ManagerRegistry $doctrine, int $id): Response
+    {
+        $professionnel = $doctrine->getRepository(Professionnel::class)->find($id);
+
+        if (!$professionnel) {
+            throw $this->createNotFoundException('Aucun professionnel trouvé avec l\'ID ' . $id);
+        }
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($professionnel);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_professionnel_lister');
+    }
 }
