c4d99a5fe3bfd41c6c3f89b815f733686582a5a7
Mahdi <mahdi.ashuri14@gmail.com>
2024-12-02 16:05:06 +0100

    ajouter_prof

--- src/Controller/ProfesseurController.php
+++ src/Controller/ProfesseurController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Professeur;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
-use App\Form\InstrumentFormType;
-use App\Form\InstrumentModifierType;
+use App\Form\ProfesseurType;
+use App\Form\ProfesseurModifierType;
 class ProfesseurController extends AbstractController
 {
     public function index(): Response
@@ -38,6 +38,26 @@ class ProfesseurController extends AbstractController
         
         return $this->render('professeur/consulter.html.twig', [
             'professeur' => $professeur,
+        ]);
+    }
+    public function ajouter(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $professeur = new Professeur();
+        $form = $this->createForm(ProfesseurType::class, $professeur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($professeur);
+            $entityManager->flush();
+
+            return $this->render('professeur/consulter.html.twig', [
+                'professeur' => $professeur,
+            ]);
+        }
+
+        return $this->render('professeur/ajouter.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
