a0a7ce3f215b32ea2644194dd5aa7d19eace0402
Mahdi <mahdi.ashuri14@gmail.com>
2024-12-02 11:52:53 +0100

    lister model marque accessoire

--- src/Controller/AccessoireController.php
+++ src/Controller/AccessoireController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Accessoire; 
+use Symfony\Component\HttpFoundation\Request;
 
 class AccessoireController extends AbstractController
 {
-    #[Route('/accessoire', name: 'app_accessoire')]
     public function index(): Response
     {
         return $this->render('accessoire/index.html.twig', [
             'controller_name' => 'AccessoireController',
         ]);
+    }
+    public function lister(ManagerRegistry $doctrine)
+    {
+        $accessoire = $doctrine->getRepository(Accessoire::class)->findAll();
+
+        return $this->render('accessoire/lister.html.twig', [
+            'paccessoire' => $accessoire,
+        ]);	
     }
 }
