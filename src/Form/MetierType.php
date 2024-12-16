5b36600b076310cfed33ea46c1437917bb51b67f
BrianDUPONT <brian-dupont@hotmail.com>
2024-12-16 14:50:22 +0100

    Création du formulaire modifier et ajouter de metier

---
+++ src/Form/MetierType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Metier;
+use App\Entity\Professionnel;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MetierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('professionnel', EntityType::class, [
+                'class' => professionnel::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+            ])
+            ->add('enregistrer', SubmitType::class, [
+                'label' => 'Nouvelle intervention',
+                'attr' => ['class' => 'btn btn-primary'] // Ajout de la classe CSS pour le bouton
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Metier::class,
+        ]);
+    }
+}
