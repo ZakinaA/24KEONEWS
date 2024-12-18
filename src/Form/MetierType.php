d15102d37b7114638d33cbc69c752ec68180f628
BrianDUPONT <165259011+BrianDUPONT@users.noreply.github.com>
2024-12-18 09:09:24 +0100

    Update MetierType.php

--- src/Form/MetierType.php
+++ src/Form/MetierType.php
@@ -7,6 +7,7 @@ use App\Entity\Professionnel;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,18 +16,20 @@ class MetierType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('professionnel', EntityType::class, [
-                'class' => professionnel::class,
-                'choice_label' => 'nom',
-                'multiple' => true,
-            ])
-            ->add('enregistrer', SubmitType::class, [
-                'label' => 'Nouveau Métier',
-                'attr' => ['class' => 'btn btn-primary']
-            ]);
+        ->add('libelle', TextType::class, [
+            'attr' => ['class' => 'form-control', 'placeholder' => 'Libellé']
+        ])
+        ->add('professionnel', EntityType::class, [
+            'class' => professionnel::class,
+            'choice_label' => 'nom',
+            'multiple' => true,
+            'attr' => ['class' => 'form-control']
+        ])
+        ->add('enregistrer', SubmitType::class, [
+            'label' => 'Nouveau métier',
+            'attr' => ['class' => 'btn btn-primary']
+        ]);
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
