d910871b2d0e760b5f30780b595cd3d44588745b
BrianDUPONT <165259011+BrianDUPONT@users.noreply.github.com>
2024-12-18 09:10:37 +0100

    Update InscriptionType.php

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -7,6 +7,8 @@ use App\Entity\Eleve;
 use App\Entity\Inscription;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -17,19 +19,24 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateInscription')
-            ->add('cours', EntityType::class, [
-                'class' => Cours::class,
-'choice_label' => 'libelle',
-            ])
-            ->add('eleve', EntityType::class, [
-                'class' => Eleve::class,
-'choice_label' => 'nom',
+        ->add('dateInscription', DateType::class, [
+            'attr' => ['class' => 'form-control', 'placeholder' => 'Date d\'inscription']
+    ])
+    ->add('cours', EntityType::class, [
+        'class' => Cours::class,
+        'choice_label' => 'libelle',
+        'attr' => ['class' => 'form-control']
+    ])
+    ->add('eleve', EntityType::class, [
+        'class' => Eleve::class,
+        'choice_label' => 'nom',
+        'attr' => ['class' => 'form-control']
+    ])
+    ->add('enregistrer', SubmitType::class, [
+        'label' => 'Nouveau cours',
+        'attr' => ['class' => 'btn btn-primary']
+    ]);
 
-            ])
-            ->add('enregistrer', SubmitType::class, array('label' => 'Nouveau cours'))
-
-        ;
 
     }
 
