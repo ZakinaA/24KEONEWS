79e4cb71d779ccd5575c97cb0103889400e67de3
BrianDUPONT <165259011+BrianDUPONT@users.noreply.github.com>
2024-12-18 09:10:06 +0100

    Update InstrumentModifierType.php

--- src/Form/InstrumentModifierType.php
+++ src/Form/InstrumentModifierType.php
@@ -2,54 +2,46 @@
 
 namespace App\Form;
 
-use App\Entity\Instrument;
-use App\Form\InstrumentFormType;
+use App\Entity\Cours;
+use App\Entity\Eleve;
+use App\Entity\Inscription;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class InstrumentModifierType extends AbstractType
+class InscriptionModifierType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('NumSerie', IntegerType::class, [
-                'attr' => ['class' => 'form-control', 'placeholder' => 'Numéro de série']
-            ])
-            ->add('dateAchat', DateType::class, [
-                'widget' => 'single_text',
-                'format' => 'yyyy-MM-dd',
-                'attr' => ['class' => 'form-control']
-            ])
-            ->add('prixAchat', IntegerType::class, [
-                'attr' => ['class' => 'form-control', 'placeholder' => 'Prix d\'achat']
-            ])
-            ->add('utilisation', TextType::class, [
-                'attr' => ['class' => 'form-control', 'placeholder' => 'Utilisation']
-            ])
-            ->add('couleur', TextType::class, [
-                'attr' => ['class' => 'form-control', 'placeholder' => 'Couleur']
-            ])
-            ->add('enregistrer', SubmitType::class, [
-                'label' => 'Modifier Instrument',
-                'attr' => ['class' => 'btn btn-primary']
-            ]);
-    }
+        ->add('dateInscription', DateType::class, [
+            'attr' => ['class' => 'form-control', 'placeholder' => 'Date d\'inscription']
+    ])
+    ->add('cours', EntityType::class, [
+        'class' => Cours::class,
+        'choice_label' => 'libelle',
+        'attr' => ['class' => 'form-control']
+    ])
+    ->add('eleve', EntityType::class, [
+        'class' => Eleve::class,
+        'choice_label' => 'nom',
+        'attr' => ['class' => 'form-control']
+    ])
+    ->add('enregistrer', SubmitType::class, [
+        'label' => 'Modifier cours',
+        'attr' => ['class' => 'btn btn-primary']
+    ]);
 
-    public function getParent()
-    {
-        return InstrumentFormType::class;
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Instrument::class,
+            'data_class' => Inscription::class,
         ]);
     }
 }
